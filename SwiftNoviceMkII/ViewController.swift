04d4f98d407cffcbe89b0173619af38500b95fe4
Noah Pope <npope@tutanota.com>
2025-07-23 03:06:43 -0500

    Initial Commit

---
+++ SwiftNoviceMkII/ViewController.swift
@@ -0,0 +1,16 @@
+//  File: ViewController.swift
+//  Project: SwiftNoviceMkII
+//  Created by: Noah Pope on 7/23/25.
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SwiftNoviceMkIITests/SwiftNoviceMkIITests.swift
@@ -0,0 +1,14 @@
+//  File: SwiftNoviceMkIITests.swift
+//  Project: SwiftNoviceMkIITests
+//  Created by: Noah Pope on 7/23/25.
+
+import Testing
+@testable import SwiftNoviceMkII
+
+struct SwiftNoviceMkIITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
