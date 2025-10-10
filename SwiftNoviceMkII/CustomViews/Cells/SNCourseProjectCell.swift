72bb9c531f79baa342c6da15e5f2d25fad9853f2
Noah Pope <npope@tutanota.com>
2025-10-10 11:23:14 -0500

    adding course project cell to storyboard

---
+++ SwiftNoviceMkII/CustomViews/Cells/SNCourseProjectCell.swift
@@ -0,0 +1,23 @@
+//  File: SNCourseProjectCell.swift
+//  Project: SwiftNoviceMkII
+//  Created by: Noah Pope on 10/10/25.
+
+import UIKit
+
+class SNCourseProjectCell: UITableViewCell
+{
+    static let reuseID = "CourseProjectCell"
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
