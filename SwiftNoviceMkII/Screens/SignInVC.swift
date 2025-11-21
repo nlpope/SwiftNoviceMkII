520f9d01b5171bb32d91bc01c56e1a6bbd22d5ea
Noah Pope <npope@tutanota.com>
2025-11-21 10:17:40 -0600

    omitting unnecessary function call in VDL

--- SwiftNoviceMkII/Screens/SignInVC.swift
+++ SwiftNoviceMkII/Screens/SignInVC.swift
@@ -31,7 +31,7 @@ class SignInVC: UIViewController, UITextFieldDelegate
         configSignInLabel()
         configSignUpLabel()
         configForgotLabel()
-        fetchExistingUsers()
+//        fetchExistingUsers()
     }
     
     
